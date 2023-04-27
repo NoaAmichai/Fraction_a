0d65329f4bd0ff5c17b0aed8dcd1f658d94f8345
Noa Amichai <noa1254@gmail.com>
2023-04-27 09:41:01 +0300

    .

--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -10,23 +10,33 @@ namespace ariel {
 
     private:
         int _numerator, _denominator;
+
         int gcd(int num, int den);
+
         void reduce();
 
     public:
         Fraction(int num, int den);
+
         Fraction(float flt);
 
 
         friend Fraction operator+(const Fraction &fraction1, const Fraction &fraction2);
+
         friend Fraction operator-(const Fraction &fraction1, const Fraction &fraction2);
+
         friend Fraction operator*(const Fraction &fraction1, const Fraction &fraction2);
+
         friend Fraction operator/(const Fraction &fraction1, const Fraction &fraction2);
 
         friend bool operator==(const Fraction &fraction1, const Fraction &fraction2);
+
         friend bool operator>(const Fraction &fraction1, const Fraction &fraction2);
+
         friend bool operator<(const Fraction &fraction1, const Fraction &fraction2);
+
         friend bool operator>=(const Fraction &fraction1, const Fraction &fraction2);
+
         friend bool operator<=(const Fraction &fraction1, const Fraction &fraction2);
 
         Fraction &operator++();  // pre-increment ++a
@@ -35,6 +45,7 @@ namespace ariel {
         Fraction operator--(int); // post-decrement a--
 
         friend std::ostream &operator<<(std::ostream &output, const Fraction &fraction);
+
         friend std::istream &operator>>(std::istream &input, Fraction &fraction);
 
     };
